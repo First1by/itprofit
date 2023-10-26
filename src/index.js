42ac7bf8316a4de6d7ef97bab7e142123eda523f
Sergey <greysound1991@mail.ru>
2023-10-26 23:32:03 +0300

    feat: complete validation of form

--- src/index.js
+++ src/index.js
@@ -3,50 +3,45 @@ const nameInput = document.form.name;
 const emailInput = document.form.email;
 const numberInput = document.form.number;
 const messageInput = document.form.message;
+const invalidText = document.querySelectorAll('.invalid-text');
 
 let validInput;
 
 const validateEmail = (email) => {
-  // const pattern = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w)$/;
-  // return pattern.test(email);
   let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
   return regex.test(email);
 };
 
-nameInput.onchange = () => {
-  if (!nameInput.value) {
-    nameInput.classList.add('invalid');
-    console.log('Invalid name');
-  };
-  if (nameInput.value) nameInput.classList.remove('invalid');
+const validateEmptyInput = () => {
+  validInput = true;
+  for (let i = 0; i < form.length - 1; i++) {
+    if (!form[i].value) {
+      form[i].classList.add('invalid');
+      invalidText[i].innerHTML = "Заполните это поле";
+      validInput = false;
+    }
+  }
 }
 
-emailInput.onchange = () => {
-  if (!emailInput.value || !validateEmail(emailInput.value)) {
-    emailInput.classList.add('invalid');
-  };
-  if (emailInput.value && validateEmail(emailInput.value)) emailInput.classList.remove('invalid');
-}
-
-numberInput.onchange = () => {
-  if (!numberInput.value) {
-    numberInput.classList.add('invalid');
-  };
-  if (numberInput.value) numberInput.classList.remove('invalid');
-}
-
-messageInput.onchange = () => {
-  if (!messageInput.value) {
-    messageInput.classList.add('invalid');
-  };
-  if (messageInput.value) messageInput.classList.remove('invalid');
+const reset = () => {
+  for (let i = 0; i < form.length - 1; i++) {
+      form[i].classList.remove('invalid');
+      invalidText[i].innerHTML = "";
+      validInput = true;
+    }
 }
 
 form.onsubmit = (e) => {
   e.preventDefault();
+  reset();
+  validateEmptyInput();
 
   if (!nameInput.value || !emailInput.value || !numberInput.value || !messageInput.value) return;
-  if (!validateEmail(emailInput.value)) return;
+  if (!validateEmail(emailInput.value)){
+    emailInput.classList.add('invalid');
+    invalidText[1].innerHTML = "Неккоректный Email";
+    return;
+  };
 
   form.submit();
   loader.style.display = "block";
@@ -56,28 +51,43 @@ form.onsubmit = (e) => {
 
 
 
-// const validateEmptyInput = () => {
-//   validInput = true;
-//   for (let i = 0; i < form.length - 1; i++) {
-//     if (!form[i].value) {
-//       form[i].classList.add('invalid');
-//       validInput = false;
-//     } else {
-//       form[i].classList.remove('invalid');
-//     }
-//   }
+
+
+// nameInput.onchange = () => {
+//   if (!nameInput.value) {
+//     nameInput.classList.add('invalid');
+//     console.log('Invalid name');
+//     invalidText[0].classList.remove('d-none');
+//   };
+//   if (nameInput.value) {
+//     nameInput.classList.remove('invalid');
+//     invalidText[0].classList.add('d-none');
+//   };
 // }
 
-// form.onsubmit = (e) => {
-//   e.preventDefault();
-//   validateEmptyInput();
+// emailInput.onchange = () => {
+//   if (!emailInput.value || !validateEmail(emailInput.value)) {
+//     emailInput.classList.add('invalid');
+//     invalidText[1].classList.remove('d-none');
+//   };
+//   if (emailInput.value && validateEmail(emailInput.value)) {
+//     emailInput.classList.remove('invalid')};
+// }
 
-//   if (!validInput) return;
-//   if (!validateEmail(emailInput.value)) return;
+// numberInput.onchange = () => {
+//   if (!numberInput.value) {
+//     numberInput.classList.add('invalid');
+//     numberInput.classList.remove('d-none');
+//   };
+//   if (numberInput.value) numberInput.classList.remove('invalid');
+// }
 
-//   form.submit();
-
-//   loader.style.display = "block";
+// messageInput.onchange = () => {
+//   if (!messageInput.value) {
+//     messageInput.classList.add('invalid');
+//     messageInput.classList.add('invalid');
+//   };
+//   if (messageInput.value) messageInput.classList.remove('invalid');
 // }
 
 
